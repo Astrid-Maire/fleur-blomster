7bffe362f27052349170973a2d272a91c1b58db0
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2025-06-05 22:06:46 +0200

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-export default {
+const nextConfig = {
   reactStrictMode: true,
+
   images: {
-    domains: ["api.mydomain.com", "cdn.something.com"],
+    domains: [
+      "dit-api-domæne.dk", // udskift med domænet hvor billederne hostes
+      "another-domain.com", // hvis der er flere domæner
+    ],
   },
 };
+
+export default nextConfig;
