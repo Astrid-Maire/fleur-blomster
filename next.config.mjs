75a3564fea78da468e4e6c7ceba81f16a0415f9e
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2025-06-05 22:25:50 +0200

    r

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
-    domains: ["xraaztpjtcujqbtvczfb.supabase.co"],
+    domains: [
+      "xraaztpjtcujqbtvczfb.supabase.co", // Billed-serveren
+    ],
   },
 };
 
